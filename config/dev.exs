8c8c7d375f7afbdd1bac4ef975024e262da67135
corka149 <corka149@mailbox.org>
2020-08-05 21:18:31 +0200

    Added dev config

--- config/dev.exs
+++ config/dev.exs
@@ -2,3 +2,5 @@ use Mix.Config
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+
+config :whoami, :env_msg, "Test fake environment variable message"
