6ec08c6df4b3dc66c215229d095939233291ceb3
corka149 <corka149@mailbox.org>
2023-02-20 07:32:59 +0100

    First try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Serialize;
+use warp::http::StatusCode;
+use warp::Filter;
+
+/**
+%{
+  host: get_host(),
+  ipv4_addresses: get_ipv4_adresses(),
+  env_msg: Application.fetch_env!(:whoami, :env_msg)
+}
+ */
+#[derive(Serialize)]
+struct Stats {
+    host: String,
+    ipv4_addresses: String,
+    env_msg: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::any().then(warp::reply::with_status("NOT FOUND", StatusCode::NOT_FOUND));
+
+    warp::serve(routes).run(([0, 0, 0, 0], 4000)).await;
 }
